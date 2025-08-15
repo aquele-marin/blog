746a709b730814a701fe4188ba474a000d335044
Gabriel Marin <gabrielvbmarin21@gmail.com>
2025-08-14 22:57:44 -0300

    feat: add drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/ui";
+import { Drawer } from "@/ui/drawer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Drawer />
     </>
   );
 }
---
+++ src/ui/drawer.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+export function Drawer() {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* Page content here */}
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          {/* Sidebar content here */}
+          <li>
+            <Link href="/">Sidebar Item 1</Link>
+          </li>
+          <li>
+            <Link href="/">Sidebar Item 2</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
